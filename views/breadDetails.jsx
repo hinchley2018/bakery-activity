ed84c45dea75110266a45c4e4428cf1b075ddc59
Jonathon Hinchley <jonathon.hinchley18@gmail.com>
2021-12-22 20:05:27 -0600

    make sure i dont lose breads i create

---
+++ views/breadDetails.jsx
@@ -0,0 +1,8 @@
+const React = require('react')
+const DefaultLayout = require("./layouts/default")
+
+function breadDetails(){
+    return (<div>Bread details</div>)
+}
+
+module.exports = breadDetails
---
+++ views/submitBread.jsx
@@ -0,0 +1,12 @@
+const React = require('react')
+const DefaultLayout = require("./layouts/default")
+
+function submitBread(){
+    return (
+        <DefaultLayout>
+            <h2>Submit bread form</h2>
+        </DefaultLayout>
+    )
+}
+
+module.exports = submitBread
