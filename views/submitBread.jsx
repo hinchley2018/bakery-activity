c9341d1be0895c0bef038f5e8f2079ff0644cc36
Jonathon Hinchley <jonathon.hinchley18@gmail.com>
2021-12-22 20:20:08 -0600

    new bread form

--- views/submitBread.jsx
+++ views/submitBread.jsx
@@ -1,10 +1,26 @@
 const React = require('react')
 const DefaultLayout = require("./layouts/default")
 
-function submitBread(){
+function submitBread() {
     return (
         <DefaultLayout>
-            <h2>Submit bread form</h2>
+            <h2>Add a new bread</h2>
+            <a href='/breads'> Go back to index</a>
+            <form
+                method='POST'
+                action="/breads"
+            >
+                <label htmlFor='name'>Name</label>
+                <input type="text" name="name" id="name" required />
+                <br />
+                <label htmlFor='image'>Image url</label>
+                <input type="text" name="image" id="image" required/>
+                <br />
+                <label htmlFor='hasGluten'>Has Gluten?</label>
+                <input type="checkbox" name="hasGluten" id="hasGluten" defaultChecked />
+                <br/>
+                <input type="submit"/>
+            </form>
         </DefaultLayout>
     )
 }
