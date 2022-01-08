548ed151df4c4c722ddb289edef6651518f0c62b
jeremyrrose <jeremy.r.rose@gmail.com>
2022-01-08 14:23:41 -0500

    quick in-class Mongoose implementtations for GET breads, GET breads/:breadId, POST breads, GET breads/:breadId/edit, PUT breads/:breadId

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -1,15 +1,24 @@
 const express = require('express')
 const router = express.Router()
 const breadsList = require('../models/breads')
+const breadModel = require('../models/bread')
 
 // This route renders a list of breads
 // 'index' is the name of the file in our views folder to render as this view
 // {breadsList: breadsList} passes information that we can use in the 'index' view
 //GET /breads -> view
 router.get('/', (req, res) => {
-    res.render('breads', {
-        breadsList: breadsList
-    })
+
+    breadModel.find()
+        .then(breads => {
+            res.render('breads', {
+                    breadsList: breads
+                })
+        })
+
+    // res.render('breads', {
+    //     breadsList: breadsList
+    // })
 })
 
 //GET /breads/new -> view
@@ -18,27 +27,52 @@ router.get("/new", (req, res) => {
 })
 
 //GET /breads/:breadIndex -> view
-router.get('/:breadIndex', (req, res) => {
-    const breadIndex = req.params.breadIndex
+router.get('/:breadId', (req, res) => {
+    const breadId = req.params.breadId
     //if it exists
-    const bread = breadsList[breadIndex]
-    if(bread){
-        res.render('breads/breadDetails', {
-            bread: bread,
-            index: breadIndex
+    breadModel.findById(breadId)
+        .then(result => {
+            console.log(result)
+            res.render('breads/breadDetails', {
+                bread: result,
+                index: breadId
+            })
         })
-    }
-    else {
-        res.render('error404')
-    }
+    // const bread = breadsList[breadId]
+    // if(bread){
+    //     res.render('breads/breadDetails', {
+    //         bread: bread,
+    //         index: breadId
+    //     })
+    // }
+    // else {
+    //     res.render('error404')
+    // }
 })
 
-router.get('/:breadIndex/edit', (req,res) => {
-    const props = {
-        bread: breadsList[req.params.breadIndex],
-        index: req.params.breadIndex
-    }
-    res.render('breads/editBread', props)
+router.get('/:breadId/edit', (req,res) => {
+    const breadId = req.params.breadId
+    //if it exists
+    breadModel.findById(breadId)
+        .then(result => {
+            console.log(result)
+            if (result) {
+
+                console.log(result)
+                res.render('breads/editBread', {
+                    bread: result,
+                    index: breadId
+                })
+            } else {
+                res.send(404)
+            }
+        })
+        .catch(err => res.send(404))
+    // const props = {
+    //     bread: breadsList[req.params.breadId],
+    //     index: req.params.breadIndex
+    // }
+    // res.render('breads/editBread', props)
 })
 
 //POST /breads <-
@@ -48,28 +82,46 @@ router.post("/", (req, res) => {
     console.log(req.body)
     //TODO: validate this is a valid bread
     //insert that bread from req.body into breads list
-    console.log("before", breadsList.length)
-    breadsList.push(req.body)
-    console.log("after", breadsList.length)
-    //either redirect to the index
-    // or send the back the id
-    res.redirect("/breads")
+    // console.log("before", breadsList.length)
+    // breadsList.push(req.body)
+    // console.log("after", breadsList.length)
 
-})
-
-//UPDATE /breads/:index <- update a bread in the breadsList
-router.put('/:breadIndex', (req, res) => {
     if (req.body.hasGluten === "on") {
         req.body.hasGluten = true
     } else {
         req.body.hasGluten = false
     }
-    if (breadsList[req.params.breadIndex]) {
-        breadsList[req.params.breadIndex] = req.body
-        res.redirect(`/breads/${req.params.breadIndex}`)
+    console.log('hi')
+
+    breadModel.create(req.body)
+        .then(result => {
+            console.log(result)
+            res.redirect("/breads")
+        })
+    //either redirect to the index
+    // or send the back the id
+    // res.redirect("/breads")
+
+})
+
+//UPDATE /breads/:index <- update a bread in the breadsList
+router.put('/:breadId', (req, res) => {
+    if (req.body.hasGluten === "on") {
+        req.body.hasGluten = true
     } else {
-        res.send(404)
+        req.body.hasGluten = false
     }
+    breadModel.findByIdAndUpdate(req.params.breadId, req.body)
+        .then(result => {
+            res.redirect(`/breads/${req.params.breadId}`)
+        })
+        .catch(err => res.send(404))
+    // if (breadsList[req.params.breadIndex]) {
+    //     breadsList[req.params.breadIndex] = req.body
+    //     res.redirect(`/breads/${req.params.breadIndex}`)
+    // } else {
+    //     res.send(404)
+    // }
 })
 
 
---
+++ models/bread.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+const {Schema} = mongoose
+
+const breadSchema = new Schema({
+    name: { type: String, required: true },
+    hasGluten: Boolean,
+    image: { type: String, default: 'http://placehold.it/500x500.png' }
+})
+
+const Bread = mongoose.model("Bread", breadSchema)
+
+module.exports = Bread
